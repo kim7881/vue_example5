b87d51bbc568411a27e59ff5c4de5c5fde8dc54c
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-02-14 17:04:47 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+      fullname : '',
+    };
+  },
+  watch: {
+    
+  },
+  computed : {
+    // fullname(){
+    //   console.log('Running again...');
+    //   if(this.name === ''){
+    //     return '';
+    //   }
+    //   return this.name + ' ' + 'Schwarzmuller'; 
+    // },
+
+  },
+  methods: {
+    outputFullname(){
+      console.log('Running again...');
+      if(this.name === ''){
+        return '';
+      }
+      return this.name + ' ' + 'Schwarzmuller'; 
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput(){
+      this.name = '';
+    }
+  }
+});
+
+app.mount('#events');
